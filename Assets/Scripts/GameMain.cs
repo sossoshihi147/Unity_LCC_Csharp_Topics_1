84c2aa006c9bd2a7056b1d83b8e8f98a2ec77cbb
sossoshihi147 <sossoshihi147@gmail.com>
2025-03-12 11:06:16 +0800

    Initial commit

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace OLIVER
+{
+    /// <summary>
+    /// 主腳本
+    /// </summary>
+    public class GameMain : MonoBehaviour
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity_LCC_Csharp_Topics
+ 聯成_C#專題_精生今世
