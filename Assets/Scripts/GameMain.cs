0018bdb2d123eaae83112f7bbd26d7000660a6d1
sossoshihi147 <sossoshihi147@gmail.com>
2025-03-12 15:46:49 +0800

    Update 初步宣告

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -9,4 +9,41 @@ namespace OLIVER
     {
 
     }
+
+    public class CharacterStats
+    {
+        public int age;
+        public int ability;
+        public int money;
+        public int Sociability;
+        public int health;
+    }
+    /// <summary>
+    /// 幼年期
+    /// </summary>
+    public class childhood : CharacterStats
+    {
+
+    }
+    /// <summary>
+    /// 少年期
+    /// </summary>
+    public class juvenile : CharacterStats
+    {
+
+    }
+    /// <summary>
+    /// 青年期
+    /// </summary>
+    public class youth : CharacterStats
+    {
+
+    }
+    /// <summary>
+    /// 成人期
+    /// </summary>
+    public class adult : CharacterStats
+    {
+
+    }
 }
