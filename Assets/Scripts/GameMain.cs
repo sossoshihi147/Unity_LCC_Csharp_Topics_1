a7cc24114d349e11232187d46d4ccb20e365c221
sossoshihi147 <sossoshihi147@gmail.com>
2025-03-19 15:36:38 +0800

    Update 宣告開頭

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -7,43 +7,106 @@ namespace OLIVER
     /// </summary>
     public class GameMain : MonoBehaviour
     {
-
-    }
-
-    public class CharacterStats
-    {
+        [SerializeField, Header("年齡")]
         public int age;
+        [SerializeField, Header("能力")]
         public int ability;
-        public int money;
-        public int Sociability;
+        [SerializeField, Header("社交")]
+        public int sociability;
+        [SerializeField, Header("健康")]
         public int health;
-    }
-    /// <summary>
-    /// 幼年期
-    /// </summary>
-    public class childhood : CharacterStats
-    {
+        [SerializeField, Header("金錢")]
+        public int money;
 
-    }
-    /// <summary>
-    /// 少年期
-    /// </summary>
-    public class juvenile : CharacterStats
-    {
+        private void Awake()
+        {
+            LogSystem.LogWithColor("恭喜你終於破防了，精生也請多多指教!!", "#f0f");
+            int randomStart = Random.Range(0, 4);
+            // 熱愛戶外之家
+            if (randomStart == 0)
+            {
+                age = 1;
+                ability = 0;
+                sociability = 10;
+                health = 120;
+                money = 800;
+                LogSystem.LogWithColor("今世你出生在熱愛戶外活動的家庭", "#f99");
+            }
+            // 大財團
+            if (randomStart == 1)
+            {
+                age = 1;
+                ability = 5;
+                sociability = 5;
+                health = 100;
+                money = 10000;
+                LogSystem.LogWithColor("今世你是大財團的三公子", "#f99");
+            }
+            // 平民
+            if (randomStart == 2)
+            {
+                age = 1;
+                ability = 0;
+                sociability = 0;
+                health = 100;
+                money = 500;
+                LogSystem.LogWithColor("今世你出生在一般的上班族家庭", "#f99");
+            }
+            // 平民
+            if (randomStart == 3)
+            {
+                age = 1;
+                ability = 10;
+                sociability = 0;
+                health = 100;
+                money = 1500;
+                LogSystem.LogWithColor("今世你出生電競世家", "#f99");
+            }
+            childhood child1 = new childhood();
+            child1.test();
+        }
 
-    }
-    /// <summary>
-    /// 青年期
-    /// </summary>
-    public class youth : CharacterStats
-    {
 
-    }
-    /// <summary>
-    /// 成人期
-    /// </summary>
-    public class adult : CharacterStats
-    {
 
+        public class CharacterStats
+        {
+            public int age;
+            public int ability;
+            public int sociability;
+            public int health;
+            public int money;
+            
+        }
+        /// <summary>
+        /// 幼年期
+        /// </summary>
+        public class childhood : CharacterStats
+        {
+            public void test()
+            {
+                LogSystem.LogWithColor($"數值{age}, {ability}, {sociability}, {health}, {money}", "#f00");
+            }
+        }
+        /// <summary>
+        /// 少年期
+        /// </summary>
+        public class juvenile : CharacterStats
+        {
+
+        }
+        /// <summary>
+        /// 青年期
+        /// </summary>
+        public class youth : CharacterStats
+        {
+
+        }
+        /// <summary>
+        /// 成人期
+        /// </summary>
+        public class adult : CharacterStats
+        {
+
+        }
     }
 }
