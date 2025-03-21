38b61831f43402bb22c348f3b5a3c9c3155ef04e
sossoshihi147 <sossoshihi147@gmail.com>
2025-03-21 09:11:48 +0800

    Update 中文及幼年事件完成

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -1,111 +1,158 @@
-﻿using UnityEngine;
+﻿using UnityEditor.Playables;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace OLIVER
 {
+    /// <summary>
+    /// 人物屬性
+    /// </summary>
+    [System.Serializable]
+    public class CharacterStats
+    {
+        public float age;
+        public float ability;
+        public float sociability;
+        public float health;
+        public float money;
+
+        public CharacterStats(float _age, float _ability, float _sociability,float _health, float _money)
+        {
+            age = _age; 
+            ability = _ability;
+            sociability = _sociability;
+            health = _health;
+            money = _money;
+        }
+
+        public void ModifyStat(string statName, int value)
+        {
+            switch (statName)
+            {
+                case "age": age += value; break;
+                case "ability": ability += value; break;
+                case "sociability": sociability += value; break;
+                case "health": health += value; break;
+                case "money": money += value; break;
+            }
+        }
+    }
     /// <summary>
     /// 主腳本
     /// </summary>
     public class GameMain : MonoBehaviour
     {
-        [SerializeField, Header("年齡")]
-        public int age;
-        [SerializeField, Header("能力")]
-        public int ability;
-        [SerializeField, Header("社交")]
-        public int sociability;
-        [SerializeField, Header("健康")]
-        public int health;
-        [SerializeField, Header("金錢")]
-        public int money;
-
+        [SerializeField] public CharacterStats stats;
         private void Awake()
         {
             LogSystem.LogWithColor("恭喜你終於破防了，精生也請多多指教!!", "#f0f");
+            Born();
+        }
+
+        private void Born()
+        {
             int randomStart = Random.Range(0, 4);
-            // 熱愛戶外之家
             if (randomStart == 0)
             {
-                age = 1;
-                ability = 0;
-                sociability = 10;
-                health = 120;
-                money = 800;
+                stats = new CharacterStats(1, 0, 10, 120, 800);
                 LogSystem.LogWithColor("今世你出生在熱愛戶外活動的家庭", "#f99");
             }
-            // 大財團
             if (randomStart == 1)
             {
-                age = 1;
-                ability = 5;
-                sociability = 5;
-                health = 100;
-                money = 10000;
+                stats = new CharacterStats(1, 5, 5, 100, 10000);
                 LogSystem.LogWithColor("今世你是大財團的三公子", "#f99");
             }
-            // 平民
             if (randomStart == 2)
             {
-                age = 1;
-                ability = 0;
-                sociability = 0;
-                health = 100;
-                money = 500;
+                stats = new CharacterStats(1, 0, 0, 100, 500);
                 LogSystem.LogWithColor("今世你出生在一般的上班族家庭", "#f99");
             }
-            // 平民
             if (randomStart == 3)
             {
-                age = 1;
-                ability = 10;
-                sociability = 0;
-                health = 100;
-                money = 1500;
+                stats = new CharacterStats(1, 10, 0, 100, 1500);
                 LogSystem.LogWithColor("今世你出生電競世家", "#f99");
             }
-            childhood child1 = new childhood();
-            child1.test();
         }
 
-
-
-        public class CharacterStats
+        public void TriggerEvent(string eventName)
         {
-            public int age;
-            public int ability;
-            public int sociability;
-            public int health;
-            public int money;
-            
-        }
-        /// <summary>
-        /// 幼年期
-        /// </summary>
-        public class childhood : CharacterStats
-        {
-            public void test()
+            stats.ModifyStat("age", 1);
+            float ageCheck = stats.age / 4;
+            switch (eventName)
             {
-                LogSystem.LogWithColor($"數值{age}, {ability}, {sociability}, {health}, {money}", "#f00");
+                case "Learn":
+                    #region 幼年時期
+                    // 幼年時期
+                    if (ageCheck < 1)
+                    {
+                        LogSystem.LogWithColor("流著口水，聽著古典樂就成長了", "#f09");
+                    }
+                    else if (ageCheck > 1 && ageCheck < 2 && ageCheck != 1)
+                    {
+                        LogSystem.LogWithColor("在幼稚園捏泥巴，玩玩具就長大了", "#f09");
+                    }
+                    if (ageCheck < 2 && ageCheck != 1)
+                    {
+                        stats.ModifyStat("ability", 2);
+                        stats.ModifyStat("money", -80);
+                    } 
+                    #endregion
+                    break;
+                case "Social":
+                    #region 幼年時期
+                    // 幼年時期
+                    if (ageCheck < 1)
+                    {
+                        LogSystem.LogWithColor("對著面前不知是哪位的大媽咯咯笑", "#f09");
+                    }
+                    else if (ageCheck > 1 && ageCheck < 2 && ageCheck != 1)
+                    {
+                        LogSystem.LogWithColor("逛街時，對路上的小姐姐眨眨眼", "#f09");
+                    }
+                    if (ageCheck < 2 && ageCheck != 1)
+                    {
+                        stats.ModifyStat("sociability", 2);
+                        stats.ModifyStat("money", -100);
+                    } 
+                    #endregion
+                    break;
+                case "Work":
+                    #region 幼年時期
+                    // 幼年時期
+                    if (ageCheck < 1)
+                    {
+                        LogSystem.LogWithColor("啃著沾有奶粉的手手被直播了", "#f09");
+                    }
+                    else if (ageCheck > 1 && ageCheck < 2 && ageCheck != 1)
+                    {
+                        LogSystem.LogWithColor("過年時，對著爺爺奶奶輸出一頓小搥搥", "#f09");
+                    }
+                    if (ageCheck < 2 && ageCheck != 1)
+                    {
+                        stats.ModifyStat("ability", 1);
+                        stats.ModifyStat("money", 400);
+                    } 
+                    #endregion
+                    break;
+                case "Rest":
+                    #region 幼年時期
+                    // 幼年時期
+                    if (ageCheck < 1)
+                    {
+                        LogSystem.LogWithColor("安安靜靜，世界和平", "#f09");
+                    }
+                    else if (ageCheck > 1 && ageCheck < 2 && ageCheck != 1)
+                    {
+                        LogSystem.LogWithColor("聽著Baby Shark，看著巧虎，終於歇停了會兒", "#f09");
+                    }
+                    if (ageCheck < 2 && ageCheck != 1)
+                    {
+                        stats.ModifyStat("health", 1);
+                        stats.ModifyStat("money", -50);
+                    } 
+                    #endregion
+                    break;
             }
-        }
-        /// <summary>
-        /// 少年期
-        /// </summary>
-        public class juvenile : CharacterStats
-        {
-
-        }
-        /// <summary>
-        /// 青年期
-        /// </summary>
-        public class youth : CharacterStats
-        {
-
-        }
-        /// <summary>
-        /// 成人期
-        /// </summary>
-        public class adult : CharacterStats
-        {
 
         }
     }
